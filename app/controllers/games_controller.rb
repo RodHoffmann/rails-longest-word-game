f279b5fc8fee9720dfd1c7b91434ede7fe05e7f9
Rodrigo Hoffmann <rhoffmannp@gmail.com>
2023-02-20 00:56:41 +0100

    Rails - ROuting, Controllers & Views - Longest Word Game with points

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,22 +8,35 @@ class GamesController < ApplicationController
   def score
     @grid_letters = params[:token].split('')
     @answer = params[:answer]
-    word_serialized = URI.open("https://wagon-dictionary.herokuapp.com/#{@answer.downcase}").read
-    word_search = JSON.parse(word_serialized)
-    word_in_dict = word_search['found']
+    word_in_dict = word_is_in_dict?
     word_in_grid = false
     @ans = ''
-    index = 0
-    @answer.upcase.chars.each do |letter|
-      index += 1 if @grid_letters.include?(letter)
-    end
-    word_in_grid = true if index == @answer.length
+    @index = 0
+    check_answer
+    check_word(word_in_dict, word_in_grid)
+  end
+
+  private
+
+  def check_word(word_in_dict, word_in_grid)
+    word_in_grid = true if @index == @answer.length
     if word_in_dict && word_in_grid
       @ans = 'correct'
     elsif word_in_grid == false
       @ans = 'not in grid'
     elsif word_in_dict == false
       @ans = 'not in dict'
+    end
+  end
+
+  def word_is_in_dict?
+    word_serialized = URI.open("https://wagon-dictionary.herokuapp.com/#{@answer.downcase}").read
+    JSON.parse(word_serialized)['found']
+  end
+
+  def check_answer
+    @answer.upcase.chars.each do |letter|
+      @index += 1 if @grid_letters.include?(letter)
     end
   end
 end
